63d1fef7a6e6b83a62eeab5c2e12180b3b38affc
Nicholas Truson <nicholas.truson@gmail.com>
2019-08-23 09:51:57 -0700

    tab nav

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -5,7 +5,64 @@ import { NavLink } from "react-router-dom";
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
-export default function TabNav() {
+// export default function TabNav() {
 
-};
+// };
 
+// *** LABELS *** //
+
+const createLabel = (iconName, labelText) => (
+    <div>
+      <Icon name={iconName} />
+      {labelText}
+    </div>
+  );
+  
+  const welcomeLabel = createLabel('home', 'Home Page');
+  const characterLabel = createLabel('users', 'Characters');
+  const locationLabel = createLabel('map', 'Locations');
+  const episodesLabel = createLabel('play', 'Episodes');
+
+// *** SEMANTIC UI PANES *** //
+
+const panes = [
+    {
+      menuItem: <Menu.Item key='home' as={Nav} to={`/`} content={welcomeLabel} />
+    },
+    {
+      menuItem: (
+        <Menu.Item
+          key='characters'
+          as={Nav}
+          to={`/characters`}
+          content={characterLabel}
+        />
+      )
+    },
+    {
+      menuItem: (
+        <Menu.Item
+          key='locations'
+          as={Nav}
+          to={`/locations`}
+          content={locationLabel}
+        />
+      )
+    },
+    {
+      menuItem: (
+        <Menu.Item
+          key='episodes'
+          as={Nav}
+          to={`/episodes`}
+          content={episodesLabel}
+        />
+      )
+    }
+  ];
+
+// *** TAB-NAV & EXPORT *** //
+
+const TabNav = () => <Tab panes={panes} renderActiveOnly={false} />;
+
+export default TabNav;
