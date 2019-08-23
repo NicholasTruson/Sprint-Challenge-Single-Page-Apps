cf3ddabff6bedf7261163a3ed64fcf3f376a73b8
Nicholas Truson <nicholas.truson@gmail.com>
2019-08-23 09:07:09 -0700

    episode list

---
+++ src/components/EpisodeList.js
@@ -0,0 +1,31 @@
+// *** ADDED IMPORTS *** //
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import EpisodeCard from './EpisodeCard';
+
+// *** EPISODE LIST *** //
+
+export default function EpisodeList() {
+    const [episodes, setEpisodes] = useState([]);
+  
+    useEffect(() => {
+      axios
+        .get('https://rickandmortyapi.com/api/episode/')
+        .then(res => {
+          console.log('episode data:', res.data.results);
+          setEpisodes(res.data.results);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }, []);
+  
+    return (
+      <section className='episode-list grid-view'>
+        {episodes.map(epi => {
+          return <EpisodeCard key={epi.id} epi={epi} />;
+        })}
+      </section>
+    );
+  }
