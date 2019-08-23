6d91a34d5e9419fca85f0df7cc8999341e3ab432
Nicholas Truson <nicholas.truson@gmail.com>
2019-08-23 08:30:41 -0700

    char card & char list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 
+// *** ADDED IMPORTS *** //
+
+
 
 export default function App() {
   return (
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,30 @@
 import React from "react";
 
-export default function CharacterCard() {
+// *** ADDED IMPORTS *** //
+
+import { Card, Icon, Image } from 'semantic-ui-react';
+
+// ** CHARACTER CARD *** //
+
+export default function CharacterCard({ char }) {
+  return (
+    <Card>
+      <Image src={char.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{char.name}</Card.Header>
+        <Card.Meta>{char.species}</Card.Meta>
+        <Card.Description>{char.location.name}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          episodes
+        </a>
+      </Card.Content>
+    </Card>
+  );
+  }
+  
+/* export default function CharacterCard() {
   return <span>todo: character</span>;
-}
+} */
