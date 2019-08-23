e82cbc65c3298f16bb6d34002ab657a1ee803902
Nicholas Truson <nicholas.truson@gmail.com>
2019-08-23 08:48:26 -0700

    location card & location list

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,26 @@
 import React from "react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
+// *** ADDED IMPORTS *** //
+
+import { Card } from 'semantic-ui-react';
+
+// *** LOCATION CARD *** //
+
+export default function LocationCard({ local }) {
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{local.name}</Card.Header>
+        <Card.Meta>{local.type}</Card.Meta>
+        <Card.Meta>{local.dimension}</Card.Meta>
+        <Card.Description> {local.created}</Card.Description>
+      </Card.Content>
+    </Card>
+  );
+  }
+
+/* export default function LocationCard({ name, type, dimension, residents }) {
   return <span>todo: location</span>;
-}
+} */
+
+
